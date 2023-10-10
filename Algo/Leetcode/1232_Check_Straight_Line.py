1023d88d256afada966ed103f5ae28fcbe5d62a1
ZoDSeR SyLVaN <zodser97@gmail.com>
2023-10-09 20:33:43 -0400

    Add explaination

--- Algo/Leetcode/1232_Check_Straight_Line.py
+++ Algo/Leetcode/1232_Check_Straight_Line.py
@@ -12,6 +12,9 @@ class Solution:
     def checkStraightLine(self, coordinates: list[list[int]]) -> bool:
         (x1, y1), (x2, y2) = coordinates[0], coordinates[1]
         for x3, y3 in coordinates[2:]:
+            # Slope 1: (y2 - y1) / (x2 - x1)
+            # Slope 2: (y3 - y1) / (x3 - x1)
+            # Need Slope 1 == Slope 2
             if (y2 - y1) * (x3 - x1) != (y3 - y1) * (x2 - x1):
                 return False
         return True
